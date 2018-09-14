8fc386da5f8bfc0dcaf7a0b98ec78e1bb2d48d38
anik1606 <anik1606@gmail.com>
2018-09-14 20:20:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    var person = katzDeliLine[i];
+    var position = i + 1;
+    var greeting = 'Welcome, ' + person + '.'
+    + ' You are number ' + position + ' in line.';
+  }
+  return greeting;
+}
+
 function nowServing(katzDeliLine) {
-  var personServed = katzDeliLine[0];
-  return 'Currently serving' + personServed;
-  katzDeliLine.splice(0, 1);
-  return katzDeliLine;
-  
+  if (katzDeliLine === []) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    var personServed = katzDeliLine[0];
+    return 'Currently serving ' + personServed;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var newLine = katzDeliLine.splice(1);
+  console.log('The Line is currently:')
+  for (var i = 0; i < newLine.length; i++) {
+    var person = newLine[i];
+    var position = i + 1;
+    var onLine = position + '. ' + person;
+    console.log(onLine);
+  }
 }
