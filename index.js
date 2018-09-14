f95b14b9158ae8f87629bc35d55e7102be59252d
anik1606 <anik1606@gmail.com>
2018-09-14 18:45:15 +0000

    automatically backed up by learn

--- index.js
+++
@@ -1,9 +0,0 @@
-function takeANumber(katzDeliLine, name) {
-  var katzDeliLine = [];
-  katzDeliLine.push(name);
-    var person = katzDeliLine[i];
-    var position = i + 1;
-    var greeting = 'Welcome, ' + person + '.'
-                    + ' You are number ' + position + ' in line.';
-  return greeting;
-}
