7d2fdbf49939b739411b57841d6cb168e01d0737
anik1606 <anik1606@gmail.com>
2018-09-14 18:21:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var katzDeliLine = [];
+function takeANumber(katzDeliLine, name) {
+  ketzDeliLine.push(name);
+  for (var i = 0; i < ketzDeliLine.length; i++) {
+    var person = ketzDeliLine[i];
+    var position = i + 1;
+    var greeting = 'Welcome, ' + person + '.' + ' You are number ' +
+  }
+}
